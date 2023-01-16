2e799395c650c5f4985fafc1e34f730b0d9b5f61
Maby <maby200@gmail.com>
2023-01-16 18:02:05 -0500

    changed .swp file

--- lambda_function.py
+++ lambda_function.py
@@ -21,22 +21,6 @@ def lambda_handler(event, context):
                 },
             },
         }
-    
-    #payment_data = {
-    #    "transaction_amount": float(bodyGet["transaction_amount"]),
-    #    "token": request.POST.get("token"),
-    #    "description": request.POST.get("description"),
-    #    "installments": int(request.POST.get("installments")),
-    #    "payment_method_id": request.POST.get("payment_method_id"),
-    #    "payer": {
-    #        "email": request.POST.get("cardholderEmail"),
-    #        "identification": {
-    #            "type": request.POST.get("identificationType"),
-    #            "number": request.POST.get("identificationNumber")
-    #        }
-    #        "first_name": request.POST.get("cardholderName")
-    #    }
-    #}
 
     payment_response = sdk.payment().create(payment_data)
     payment = payment_response["response"]
